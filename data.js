c7192470de35536d5311cac995e5d9706824e2d7
thnammm <nam.tran@journeyh.io>
2020-07-21 17:57:18 +0700

    done js2 and js3

---
+++ data.js
@@ -0,0 +1,104 @@
+var data = [{
+        "id": 1,
+        "question": "What is your favorite colour?",
+        "answers": [
+            { "a_id": 1, "answer": "Red Chótttt", "affect": 2 },
+            { "a_id": 2, "answer": "Purple Lịm Find Sim", "affect": 3 },
+            { "a_id": 3, "answer": "Yellow Khè", "affect": 4 }
+        ]
+    },
+    {
+        "id": 2,
+        "question": "Which meal do you choose for your dinner?",
+        "answers": [
+            { "a_id": 1, "answer": "Samyang Noodle", "affect": 5 },
+            { "a_id": 2, "answer": "King Crab", "affect": 6 },
+            { "a_id": 3, "answer": "Tomatoes Chiên Giòn", "affect": 7 }
+        ]
+    },
+    {
+        "id": 3,
+        "question": "Which cloth do you prefer to wear for your best friend's wedding?",
+        "answers": [
+            { "a_id": 1, "answer": "Bikini Sông Biển", "affect": 8 },
+            { "a_id": 2, "answer": "Gentle Sơ-mi", "affect": 9 },
+            { "a_id": 3, "answer": "Trần trụi with nature", "affect": 10 }
+        ]
+    },
+    {
+        "id": 4,
+        "question": "Which place do you want to travel with your company most",
+        "answers": [
+            { "a_id": 1, "answer": "Đà Lạt heeee", "affect": 11 },
+            { "a_id": 2, "answer": "Vĩnh Hy bitch", "affect": 12 }
+        ]
+    },
+    {
+        "id": 5,
+        "question": "Who do you want to eat with?",
+        "answers": [
+            { "a_id": 1, "answer": "Colleagues", "affect": 8 },
+            { "a_id": 2, "answer": "Friends", "affect": 9 },
+            { "a_id": 3, "answer": "Family", "affect": 10 },
+            { "a_id": 4, "answer": "Alone please :(", "affect": 11 }
+        ]
+    },
+    {
+        "id": 6,
+        "question": "What do you expect from your lovers?",
+        "answers": [
+            { "a_id": 1, "answer": "Exciter Player", "affect": 11 },
+            { "a_id": 2, "answer": "Vin Car I love VietNam", "affect": 12 }
+        ]
+    },
+    {
+        "id": 7,
+        "question": "Ques7",
+        "answers": [
+            { "a_id": 1, "answer": "Answer here", "affect": 8 },
+            { "a_id": 2, "answer": "Answer here", "affect": 9 },
+            { "a_id": 3, "answer": "Answer here", "affect": 10 }
+        ]
+    },
+    {
+        "id": 8,
+        "question": "Ques8",
+        "answers": [
+            { "a_id": 1, "answer": "Answer here", "affect": 2 },
+            { "a_id": 2, "answer": "Answer here", "affect": 4 }
+        ]
+    },
+    {
+        "id": 9,
+        "question": "Ques9",
+        "answers": [
+            { "a_id": 1, "answer": "Answer here", "affect": 5 },
+            { "a_id": 2, "answer": "Answer here", "affect": 6 },
+            { "a_id": 3, "answer": "Answer here", "affect": 7 }
+        ]
+    },
+    {
+        "id": 10,
+        "question": "Ques10",
+        "answers": [
+            { "a_id": 1, "answer": "Answer here", "affect": 2 },
+            { "a_id": 2, "answer": "Answer here", "affect": 4 }
+        ]
+    },
+    {
+        "id": 11,
+        "question": "Ques11",
+        "answers": [
+            { "a_id": 1, "answer": "Answer here", "affect": 3 },
+            { "a_id": 2, "answer": "Answer here", "affect": 9 }
+        ]
+    },
+    {
+        "id": 12,
+        "question": "Ques12",
+        "answers": [
+            { "a_id": 1, "answer": "Answer here", "affect": 0 },
+            { "a_id": 2, "answer": "Answer here", "affect": 0 }
+        ]
+    }
+]
